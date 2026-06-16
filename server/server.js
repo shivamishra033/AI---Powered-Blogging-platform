1b3e6e3743a8f05f2c2586c5e8d1d7a589829f16
Shivam Mishra <shivammishra@Shivams-MacBook-Air-9136.local>
2026-06-17 04:53:32 +0530

    Force CORS headers

--- server/server.js
+++ server/server.js
@@ -10,6 +10,17 @@ const app = express();
 
  await connectDB()
 // Middlewarea
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://ai-powered-blogging-platform-rouge.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 app.use(cors({
   origin: [
     "http://localhost:5173",
